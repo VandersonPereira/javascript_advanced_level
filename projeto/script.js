9b31811f7076a20f0b92270bd323ebf66975b719
Vanderson Pereira <vandoap@hotmail.com>
2018-10-22 17:44:13 -0300

    Code Modulation in project
    Aplied code modulation in Search in Github project.

--- projeto/script.js
+++ projeto/script.js
@@ -1,56 +1,82 @@
-const listElement = document.querySelector('#list');
-const searchInput = document.querySelector('#search');
-let languageSelect = document.querySelector('#language-tags');
-let languageTag = 'en-US';
+const Template = (function(){
+    let languageSelect = document.querySelector('#language-tags');
+    const listElement = document.querySelector('#list');
 
-let listItems = [
-    {
-        full_name: 'Javascript 1',
-        created_at: '2020-08-25T20:10:50Z',
-        forks: 15300
-    },
-    {
-        full_name: 'Javascript 2',
-        created_at: '2020-07-25T20:10:50Z',
-        forks: 17888
-    },
-    {
-        full_name: 'Javascript 3',
-        created_at: '2018-07-25T20:10:50Z',
-        forks: 998452
+    let listItems = [];
+
+    let languageTag = 'en-US';
+
+    languageSelect.addEventListener('change', changeLanguage);
+    
+    function changeLanguage(){
+        languageTag = languageSelect.value;
+        render();
     }
-];
 
-languageSelect.addEventListener('change', changeLanguage);
+    function setList(list){
+        listItems = list;
+        render();
+    }
+    
+    function render(){
+        let html ='';
+        const numberFormatter = new Intl.NumberFormat(languageTag);
+        const dateTimeFormatter = new Intl.DateTimeFormat(languageTag, {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
+    
+        listItems.forEach(item => {
+            const forks = numberFormatter.format(item.forks);
+            const creatAt = dateTimeFormatter.format( new Date(item.created_at));
+    
+            html += `
+            <li>
+                <div>
+                    <b>Name:</b> ${item.full_name}
+                </div>
+                <div>
+                    <b>Created At:</b> ${creatAt}
+                </div>
+                <div>
+                    <b>Forks:</b> ${forks}
+                </div>
+            </li>`;
+        });
+        listElement.innerHTML = html;
+    }
 
-function changeLanguage(){
-    languageTag = languageSelect.value;
-    render();
-}
+    return{
+        setList
+    }
 
-function render(){
-    let html ='';
-    const numberFormatter = new Intl.NumberFormat(languageTag);
-    const dateTimeFormatter = new Intl.DateTimeFormat(languageTag, {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'});
+})();
 
-    listItems.forEach(item => {
-        const forks = numberFormatter.format(item.forks);
-        const creatAt = dateTimeFormatter.format( new Date(item.created_at));
+const Data = (function($){
+    const searchInput = document.querySelector('#search');
 
-        html += `
-        <li>
-            <div>
-                <b>Name:</b> ${item.full_name}
-            </div>
-            <div>
-                <b>Created At:</b> ${creatAt}
-            </div>
-            <div>
-                <b>Forks:</b> ${forks}
-            </div>
-        </li>`;
-    });
-    listElement.innerHTML = html;
-}
+    searchInput.addEventListener('keyup', search);
 
-render();
+    function search(event){
+        if (event && event.keyCode === 13) {
+            $.setList([
+                {
+                    full_name: 'Javascript 1',
+                    created_at: '2020-08-25T20:10:50Z',
+                    forks: 15300
+                },
+                {
+                    full_name: 'Javascript 2',
+                    created_at: '2020-07-25T20:10:50Z',
+                    forks: 17888
+                },
+                {
+                    full_name: 'Javascript 3',
+                    created_at: '2018-07-25T20:10:50Z',
+                    forks: 998452
+                }
+            ]);
+        }
+    }
+
+})(Template);
+
+
+
